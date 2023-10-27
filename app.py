95f126ec4710fb8ce9059d16b2fb225172c4c46d
Vibhujay99 <vibhujay99@gmail.com>
2023-10-27 23:31:25 +0530

    Binary classify model fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson import ObjectId
 from flask import Flask, jsonify, request
 from keras.models import load_model
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 import tensorflow as tf
 from flask import Flask
 from flask_cors import CORS
@@ -55,39 +55,59 @@ def hello_world():
 @app.route("/classify_cancer", methods=['POST'])
 def classify_cancer():
     try:
-        image_file = request.files['image']
-        # get the image from request and preprocess the image
-        image = Image.open(image_file)
-        image = image.resize((64, 64))  # Resize the image to 64 x 64 pixels
-        image = np.array(image)  # Convert image to NumPy array
-        image = image / 255.0  # Normalize the pixel values to 0-1
-        image = np.expand_dims(image, axis=0)
+        # image_file = request.files['image']
+        # # get the image from request and preprocess the image
+        # image = Image.open(image_file)
+        # image = image.resize((64, 64))  # Resize the image to 64 x 64 pixels
+        # image = np.array(image)  # Convert image to NumPy array
+        # image = image / 255.0  # Normalize the pixel values to 0-1
+        # image = np.expand_dims(image, axis=0)
 
-        np.set_printoptions(suppress=True)
-
-        # Load the binary classification model
+        # Load the binary classification model first
         print("Image coming to the model")
         binary_model = load_model("models/h5/keras_model.h5", compile=False)
         print("Image left the model")
 
         # Load the labels
         class_names = open("models/h5/labels.txt", "r").readlines()
+        print(class_names)
+
+        # Process the image
+        image_file = request.files['image']
+        image = Image.open(image_file).convert("RGB")
+
+        # Resize the image to 224x224
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+        image_array = np.asarray(image)
+        normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        data[0] = normalized_image_array
 
         # Run binary classification on the image
-        binary_result = binary_model.predict(image)
+        binary_result = binary_model.predict(data)
         print(binary_result)
-        index = np.argmax(binary_result)
-        class_name = class_names[index]
-        confidence_score = binary_result[0][index]
+        binary_confidence = binary_result[0][0]
 
         # Check if the image is "SkinRelatedImages" or "RandomImages"
-        if class_name == 0 and confidence_score > 0.75:  # SkinRelatedImages
+        if binary_confidence > 0.75:  # SkinRelatedImages
             # Load the skin cancer classification model
+            print("Inside the condition")
             skin_cancer_model = tf.keras.models.load_model("models/h5/model.h5")
+            print("Model loaded")
+
+            image_file = request.files['image']
+            # get the image from request and preprocess the image
+            image = Image.open(image_file)
+            image = image.resize((64, 64))  # Resize the image to 64 x 64 pixels
+            image = np.array(image)  # Convert image to NumPy array
+            image = image / 255.0  # Normalize the pixel values to 0-1
+            image = np.expand_dims(image, axis=0)
 
             # Run the skin cancer classification model on the image and return results
             c = 0
             results = {}
+            print("results loaded")
             print(skin_cancer_model.predict(image))
             for i in skin_cancer_model.predict(image)[0]:
                 label = ['akiec', 'bcc', 'bkl', 'df', 'mel', 'nv', 'vasc'][c]
@@ -95,11 +115,12 @@ def classify_cancer():
                 c = c + 1
                 results[label] = (np.around(i * 100, decimals=2))
             return jsonify(results)
-        elif binary_result[0] == 1 and binary_result[1] > 0.75:  # RandomImages
+        else:  # RandomImages
+            print("Random Image")
             return jsonify("The image is not skin related")
-        else:
-            return jsonify("Uncertain classification")
-    except:
+
+    except Exception as e:
+        print("Error", str(e))
         return jsonify("Error!"), 500
 
 
